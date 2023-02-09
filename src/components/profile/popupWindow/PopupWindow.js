550557e50dac6920f1535693e11ce3f77a52a9f9
muzykadenys <denysmuzyka04@gmail.com>
2023-02-09 20:58:37 +0200

    registration logic, user setup, subscription popup, small fixes

---
+++ src/components/profile/popupWindow/PopupWindow.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import "../popupWindow/popupWindow.scss"
+
+function PopupWindow() {
+  return (
+    <div className='PopupWindow_Section'>
+      
+    </div>
+  )
+}
+
+export default PopupWindow
